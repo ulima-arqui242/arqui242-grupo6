d22751ee8bbba4a9db642f0193d5ab76a04d429d
NACB654 <nacb654@gmail.com>
2024-11-01 14:16:10 -0500

    Poniendo la app por separado

--- 6/vitatrack/backend/src/databases/cassandra.js
+++
@@ -1,8 +0,0 @@
-const cassandra = require("cassandra-driver");
-const config = require("../config/database");
-
-const cloud = { secureConnectBundle: config.cassandra.bundle };
-const authProvider = new cassandra.auth.PlainTextAuthProvider("token", config.cassandra.token);
-const client = new cassandra.Client({ cloud, authProvider });
-
-module.exports = client;
--- 6/vitatrack/backend/src/index.js
+++
@@ -1,6 +0,0 @@
-const app = require("./app");
-const database = require("./factory/databaseFactory");
-
-app.listen(3000, () => {
-  console.log("Escuchando en el puerto 3000");
-})
