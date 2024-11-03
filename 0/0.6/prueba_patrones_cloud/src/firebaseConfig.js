18d5564b0da70eb8c411523db64c2183b0117c34
Renzoar500 <144291148+Renzoar500@users.noreply.github.com>
2024-11-03 18:04:02 -0500

    Update firebaseConfig.js

--- 0/0.6/prueba_patrones_cloud/src/firebaseConfig.js
+++ 0/0.6/prueba_patrones_cloud/src/firebaseConfig.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
-
+///Configuración del proyecto de firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCBPyWNXelcz6AIkKnT68tbi8eYZ-h4CcY",
   authDomain: "prueba-patrones-cloud.firebaseapp.com",
@@ -10,9 +10,11 @@ const firebaseConfig = {
   appId: "1:644167211830:web:a63ce61bed9225f5f4f197",
   measurementId: "G-HNEL9EWB3H"
 };
-
+/// inicializamos el app con la configuración del proyecto
 const app = initializeApp(firebaseConfig);
+/// Función del módulo de autenticación de firebase para obtener las instancias de autenticación
 const auth = getAuth(app);
+/// Función para tener como porveedor de identidad a Google y que nos permita loguearnos con una cuenta de gmail
 const provider = new GoogleAuthProvider();
-
+/// signInWithPopup y signOut son funciones para manejar el inicio y cierre de sesión. El primero abre una ventana emergente para autenticar al usuario mediante Google y el segundo sirve para cerrar la sesión del usuario actual.
 export { auth, provider, signInWithPopup, signOut };
